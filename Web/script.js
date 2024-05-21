c0c99438e869acff534d8d2261f1a2ac8ca7af9e
Jan Náprstek <jan1nap@seznam.cz>
2024-05-21 13:32:05 +0200

    funkcni slideshow(tak nejak)

--- Web/script.js
+++ Web/script.js
@@ -1,10 +1,12 @@
 let currImg = 0
 function slide(){
-    document.getElementsByTagName("header")[0].style.backgroundImage="url('./img/img"+currImg+"')"
-    if(currImg >= 1){
+    document.getElementsByTagName("header")[0].style.backgroundImage="url('./img/img"+currImg+".jpg')"
+    currImg ++
+    console.log(currImg)
+    if(currImg > 1){
         currImg = 0
     }
-    currImg ++
+    
 }
 
 setInterval(slide, 1000)
