63f31b3b4644e9daa63ef2cfd0e4791b14281756
Jan Náprstek <jan1nap@seznam.cz>
2024-05-21 13:39:57 +0200

    added randomly selected first image in slideshow

--- Web/script.js
+++ Web/script.js
@@ -1,12 +1,13 @@
-let currImg = 0
+let images = 2
+let currImg = Math.floor(Math.random()*images)
 function slide(){
     document.getElementsByTagName("header")[0].style.backgroundImage="url('./img/img"+currImg+".jpg')"
     currImg ++
     console.log(currImg)
-    if(currImg > 1){
+    if(currImg > images - 1){
         currImg = 0
     }
     
 }
-
+slide()
 setInterval(slide, 1000)
