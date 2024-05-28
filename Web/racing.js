aee7e0a27e9dfb0123fabbb988af8982d3b8092a
Jan Náprstek <jan1nap@seznam.cz>
2024-05-28 13:48:37 +0200

    vytvoril jsem snaka s autem lo l

--- Web/racing.js
+++ Web/racing.js
@@ -5,26 +5,62 @@ const car = document.getElementById("car")
 let posX = 10
 let posY = 10
 
+let speed = 3
+let speedX = 0
+let speedY = 0
+
 let cenX = posX + 40
 let cenY = posY + 40
 let direction = 0
-
 function draw(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
     cenX = posX + 40
     cenY = posY + 40
     ctx.translate(cenX, cenY); // Posun na střed auta
     ctx.rotate((Math.PI/180)*direction)
-    ctx.translate(0, 0);
+    ctx.translate(-cenX, -cenY);
     ctx.drawImage(car, posX, posY, 80, 80)
-    ctx.fillRect(0, 0, 10, 10)
+    posX += speedX
+    posY += speedY
     
 }
 
 
-function rotate(){
-    direction++
-    posX = 0
-    posY = 0
-    draw()
+function rotate(speed){
+    direction = speed
 }
-setInterval(rotate, 200)
+
+function move(x, y){
+    speedX = x
+    speedY = y
+}
+
+function stop(){
+    rotate(0)
+    move(0, 0)
+}
+
+window.addEventListener("keydown", function (event){
+    if (event.defaultPrevented) {
+        return;
+    }
+    
+    switch(event.key){
+        case "ArrowRight":
+            move(speed, 0)
+            break
+        case "ArrowLeft":
+            move(-speed, 0)
+            break
+        case "ArrowUp":
+            move(0, -speed)
+            break
+        case "ArrowDown":
+            move(0, speed)
+            break
+    }
+}, true)
+
+setInterval(draw, 1000/60)
+    
+
