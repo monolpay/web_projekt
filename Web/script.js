99a7e227ffdee7383c45565be1cd1fcc11abf743
monolpay <jan1nap@seznam.cz>
2024-06-07 18:00:53 +0200

    added current time

--- Web/script.js
+++ Web/script.js
@@ -1,4 +1,5 @@
 let images = 6
+
 let currImg = Math.floor(Math.random()*images)
 function slide(){
     document.getElementsByTagName("header")[0].style.backgroundImage="url('./img/img"+currImg+".jpg')"
@@ -8,5 +9,14 @@ function slide(){
     }
     
 }
+
+function timeDisplay(){
+    let now = new Date
+let hours = now.getHours()
+let minutes = now.getMinutes()
+let seconds = now.getSeconds()
+    document.getElementById("time").textContent = hours+":"+minutes+":"+seconds
+}
 slide()
 setInterval(slide, 5000)
+setInterval(timeDisplay, 1000)
